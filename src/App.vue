824fc28db6fb55911ee73edc70fab03247bc07d9
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-05-15 09:34:12 +0200

    feat: init vuejs project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quod voluptatibus culpa
+    dignissimos, excepturi deleniti quidem quos optio placeat eum distinctio nostrum, nesciunt modi
+    consequuntur autem est iure blanditiis, earum aliquam? Commodi accusamus optio est repudiandae
+    necessitatibus quidem vero enim officia quam, eum exercitationem atque modi nemo nisi quos,
+    quisquam aliquid hic corporis quasi consequuntur? Ut architecto ipsum vero unde! Repellat
+    reprehenderit in tempore? Quisquam impedit ut similique voluptas illo consequatur provident
+    eaque error culpa corrupti, commodi cumque, temporibus ullam? Fugiat totam odit illo dolorem.
+    Quo, reiciendis sint error odit minus natus iste consectetur, vitae mollitia libero,
+    necessitatibus laudantium quibusdam!
+  </div>
+</template>
