412f2cf09ec8dbaa7db5a0d9527b4c39c2d0a121
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2025-05-15 09:41:19 +0200

    feat: time field with reka

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { TimeFieldInput, TimeFieldRoot } from 'reka-ui'
+</script>
 
 <template>
-  <div class="bg-pink-500 text-white">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis quod voluptatibus culpa
-    dignissimos, excepturi deleniti quidem quos optio placeat eum distinctio nostrum, nesciunt modi
-    consequuntur autem est iure blanditiis, earum aliquam? Commodi accusamus optio est repudiandae
-    necessitatibus quidem vero enim officia quam, eum exercitationem atque modi nemo nisi quos,
-    quisquam aliquid hic corporis quasi consequuntur? Ut architecto ipsum vero unde! Repellat
-    reprehenderit in tempore? Quisquam impedit ut similique voluptas illo consequatur provident
-    eaque error culpa corrupti, commodi cumque, temporibus ullam? Fugiat totam odit illo dolorem.
-    Quo, reiciendis sint error odit minus natus iste consectetur, vitae mollitia libero,
-    necessitatibus laudantium quibusdam!
+  <div class="max-w-xl mx-auto">
+    <label for="time" class="block mt-8 text-sm/6 font-medium text-gray-900">Time</label>
+    <TimeFieldRoot
+      class="mt-2 inline-flex w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+      locale="fr"
+      id="time"
+      v-slot="{ segments }"
+    >
+      <template v-for="item in segments" :key="item.part">
+        <TimeFieldInput :part="item.part">
+          {{ item.value }}
+        </TimeFieldInput>
+      </template>
+    </TimeFieldRoot>
   </div>
 </template>
